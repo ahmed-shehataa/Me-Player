d59a33b081c2bdbe79d923b94aa45da4cc03846c
ahmed shehata <ahamdyshehata7@gmail.com>
2023-08-06 16:27:24 +0300

    readAllMediaAudio

--- app/src/main/java/com/ashehata/me_player/modules/home/HomeActivity.kt
+++ app/src/main/java/com/ashehata/me_player/modules/home/HomeActivity.kt
@@ -1,9 +1,14 @@
 package com.ashehata.me_player.modules.home
 
 import android.Manifest
+import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -22,7 +27,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Permission requests
+            // TODO Permission requests
             RequestNotificationPermission()
             RequestMediaPermission()
 
@@ -35,6 +40,73 @@ class HomeActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun readAllMediaAudio() {
+        val audioList = mutableListOf<Audio>()
+
+        val collection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Audio.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL
+                )
+            } else {
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+            }
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.DISPLAY_NAME,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.SIZE
+        )
+
+        val query = contentResolver.query(
+            collection,
+            projection,
+            null,
+            null,
+            null
+        )
+
+        query?.use { cursor ->
+            // Cache column indices.
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val nameColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+            val durationColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
+
+            while (cursor.moveToNext()) {
+                // Get values of columns for a given video.
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val duration = cursor.getInt(durationColumn)
+                val size = cursor.getInt(sizeColumn)
+
+                val contentUri: Uri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                // Stores column values and the contentUri in a local object
+                // that represents the media file.
+                audioList += Audio(contentUri, name, duration, size)
+            }
+
+            Toast.makeText(this, audioList.size.toString(), Toast.LENGTH_SHORT).show()
+
+            Log.i("readAllMediaAudio", audioList.toString())
+            query.close()
+        }
+    }
+
+    data class Audio(
+        val uri: Uri,
+        val name: String,
+        val duration: Int,
+        val size: Int
+    )
 
     @Composable
     private fun RequestNotificationPermission() {
@@ -77,6 +149,7 @@ class HomeActivity : AppCompatActivity() {
             ActivityResultContracts.RequestPermission()
         ) { isGranted ->
             if (isGranted) {
+                readAllMediaAudio()
                 Toast.makeText(this, "Media isGranted", Toast.LENGTH_SHORT).show()
 
             } else {
@@ -92,6 +165,8 @@ class HomeActivity : AppCompatActivity() {
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 launcher.launch(permission)
+            } else {
+                readAllMediaAudio()
             }
         }
 
