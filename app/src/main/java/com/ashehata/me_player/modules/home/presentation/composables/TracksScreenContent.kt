49f7de3eed8ea4d5d197d8ff165c6381b782ec3e
ahmed shehata <ahamdyshehata7@gmail.com>
2023-10-11 13:55:46 +0300

    Add ScrollTopButton

--- app/src/main/java/com/ashehata/me_player/modules/home/presentation/composables/TracksScreenContent.kt
+++ app/src/main/java/com/ashehata/me_player/modules/home/presentation/composables/TracksScreenContent.kt
@@ -1,7 +1,13 @@
 package com.ashehata.me_player.modules.home.presentation.composables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -12,9 +18,12 @@ import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -28,8 +37,10 @@ import com.ashehata.me_player.modules.home.presentation.model.TracksScreenMode
 import com.ashehata.me_player.modules.home.presentation.pagination.AllTracksPagingCompose
 import com.ashehata.me_player.modules.home.presentation.pagination.FavTracksPagingCompose
 import com.ashehata.me_player.modules.home.presentation.pagination.MostPlayedTracksPagingCompose
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun TracksScreenContent(
     allTracksPagingData: AllTracksPagingCompose,
@@ -42,88 +53,142 @@ fun TracksScreenContent(
     toggleTrackToFavourite: (TrackUIModel) -> Unit,
     bottomPadding: Dp
 ) {
+    val scope = rememberCoroutineScope()
     val allTracksListState = rememberLazyListState()
     val favouriteTracksListState = rememberLazyListState()
     val mostPlayedTracksListState = rememberLazyListState()
 
+    val isScrollVisible: State<Boolean> = remember(screenMode) {
+        derivedStateOf {
+            when (screenMode) {
+                TracksScreenMode.All -> {
+                    allTracksListState.firstVisibleItemIndex > 1
+                }
 
-    Column(Modifier.background(MaterialTheme.colors.primary).padding(bottom = bottomPadding)) {
-        val tabIndex by remember(screenMode) {
-            derivedStateOf {
-                when (screenMode) {
-                    TracksScreenMode.All -> 0
-                    TracksScreenMode.Favourite -> 1
-                    TracksScreenMode.MostPlayed -> 2
+                TracksScreenMode.Favourite -> {
+                    favouriteTracksListState.firstVisibleItemIndex > 1
+                }
+
+                TracksScreenMode.MostPlayed -> {
+                    mostPlayedTracksListState.firstVisibleItemIndex > 1
                 }
             }
         }
-        val tabs = TracksScreenMode.values()
+    }
 
-        TopAppBar(
-            elevation = 4.dp,
-            title = {
-                Text(
-                    stringResource(id = R.string.app_name),
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-            },
-            backgroundColor = MaterialTheme.colors.primary,
-            actions = {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
+            .padding(bottom = bottomPadding)
+    ) {
 
-            })
-
-        TabRow(selectedTabIndex = tabIndex) {
-            tabs.forEachIndexed { index, type ->
-                Tab(text = { Text(stringResource(id = type.titleRes)) },
-                    selected = tabIndex == index,
-                    onClick = { onChangeScreenMode(type) },
-                    icon = {
-                        Icon(
-                            modifier = Modifier.size(26.dp),
-                            imageVector = ImageVector.vectorResource(id = type.iconRes),
-                            contentDescription = null
-                        )
+        Column {
+            val tabIndex by remember(screenMode) {
+                derivedStateOf {
+                    when (screenMode) {
+                        TracksScreenMode.All -> 0
+                        TracksScreenMode.Favourite -> 1
+                        TracksScreenMode.MostPlayed -> 2
                     }
-                )
+                }
             }
+            val tabs = TracksScreenMode.values()
+
+            TopAppBar(
+                elevation = 4.dp,
+                title = {
+                    Text(
+                        stringResource(id = R.string.app_name),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                backgroundColor = MaterialTheme.colors.primary,
+                actions = {
+
+                })
+
+            TabRow(selectedTabIndex = tabIndex) {
+                tabs.forEachIndexed { index, type ->
+                    Tab(text = { Text(stringResource(id = type.titleRes)) },
+                        selected = tabIndex == index,
+                        onClick = { onChangeScreenMode(type) },
+                        icon = {
+                            Icon(
+                                modifier = Modifier.size(26.dp),
+                                imageVector = ImageVector.vectorResource(id = type.iconRes),
+                                contentDescription = null
+                            )
+                        }
+                    )
+                }
+            }
+
+
+            when (screenMode) {
+                TracksScreenMode.All -> {
+                    TracksItems(
+                        tracksPagingData = allTracksPagingData,
+                        listState = allTracksListState,
+                        currentSelectedTrack = currentSelectedTrack,
+                        onTrackClicked = onTrackClicked,
+                        toggleTrackToFavourite = toggleTrackToFavourite
+                    )
+                }
+
+                TracksScreenMode.Favourite -> {
+                    TracksItems(
+                        tracksPagingData = favouriteTracksPagingData,
+                        listState = favouriteTracksListState,
+                        currentSelectedTrack = currentSelectedTrack,
+                        onTrackClicked = onTrackClicked,
+                        toggleTrackToFavourite = toggleTrackToFavourite
+
+                    )
+                }
+
+                TracksScreenMode.MostPlayed -> {
+                    TracksItems(
+                        tracksPagingData = mostPlayedTracksPagingData,
+                        listState = mostPlayedTracksListState,
+                        currentSelectedTrack = currentSelectedTrack,
+                        onTrackClicked = onTrackClicked,
+                        toggleTrackToFavourite = toggleTrackToFavourite
+
+                    )
+                }
+            }
+
+
         }
 
+        AnimatedVisibility(
+            visible = isScrollVisible.value, modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 12.dp),
+            enter = scaleIn(),
+            exit = scaleOut()
+        ) {
+            ScrollTopButton(Modifier) {
+                scope.launch {
+                    when (screenMode) {
+                        TracksScreenMode.All -> {
+                            allTracksListState.animateScrollToItem(0)
+                        }
 
-        when (screenMode) {
-            TracksScreenMode.All -> {
-                TracksItems(
-                    tracksPagingData = allTracksPagingData,
-                    listState = allTracksListState,
-                    currentSelectedTrack = currentSelectedTrack,
-                    onTrackClicked = onTrackClicked,
-                    toggleTrackToFavourite = toggleTrackToFavourite
-                )
-            }
+                        TracksScreenMode.Favourite -> {
+                            favouriteTracksListState.animateScrollToItem(0)
+                        }
 
-            TracksScreenMode.Favourite -> {
-                TracksItems(
-                    tracksPagingData = favouriteTracksPagingData,
-                    listState = favouriteTracksListState,
-                    currentSelectedTrack = currentSelectedTrack,
-                    onTrackClicked = onTrackClicked,
-                    toggleTrackToFavourite = toggleTrackToFavourite
+                        TracksScreenMode.MostPlayed -> {
+                            mostPlayedTracksListState.animateScrollToItem(0)
+                        }
+                    }
+                }
 
-                )
-            }
-
-            TracksScreenMode.MostPlayed -> {
-                TracksItems(
-                    tracksPagingData = mostPlayedTracksPagingData,
-                    listState = mostPlayedTracksListState,
-                    currentSelectedTrack = currentSelectedTrack,
-                    onTrackClicked = onTrackClicked,
-                    toggleTrackToFavourite = toggleTrackToFavourite
-
-                )
             }
         }
-
 
     }
 
